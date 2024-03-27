38ab2f7322e25dc654a370b253c10b4925f263bf
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-03-27 19:09:48 -0300

    creating a menu

--- hello.go
+++ hello.go
@@ -1,7 +1,17 @@
 package main
 
 import "fmt"
+import "reflect"
 
 func main() {
-	fmt.Println("hello world")
+	var nome string = "João"
+	fmt.Println("Olá sr.", nome, "sua idade é")
+	
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+	
+	var comando int
+	fmt.Scanf("%d", &comando)
+	fmt.Println("O comando escolhido foi", reflect.TypeOf(comando))
 }
